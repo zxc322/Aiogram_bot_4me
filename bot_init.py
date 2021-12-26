9b2d837902a7eab7552371eabed1be354c44206b
zxc_322 <91989456+zxc322@users.noreply.github.com>
2021-12-26 21:07:01 +0200

    Update bot_init.py

--- bot_init.py
+++ bot_init.py
@@ -2,13 +2,14 @@ from aiogram import Dispatcher, Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
-
+token = '5000663222:AAFZvEvHl9EmyMVen2N52UJCqQiX3MwsaY8'
 WEBHOOK_HOST = 'https://aiogram-bot-for-u.herokuapp.com'
-WEBHOOK_URL = f"{WEBHOOK_HOST}"
-WEBHOOK_PATH = ''
+WEBHOOK_PATH = '/webhook/' + token
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
 WEBAPP_HOST = 'localhost'
 WEBAPP_PORT = 3001
-token = '5000663222:AAFZvEvHl9EmyMVen2N52UJCqQiX3MwsaY8'
+
 bot = Bot(token)
 dp = Dispatcher(bot, storage=storage)
 group_chat_id = '-535090506'
