a0f6842fb86b5faf47c36c6926923d35c0e65011
zxc_322 <91989456+zxc322@users.noreply.github.com>
2021-12-26 20:12:38 +0200

    Update bot_init.py

--- bot_init.py
+++ bot_init.py
@@ -4,10 +4,11 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage = MemoryStorage()
 
 WEBHOOK_HOST = 'https://aiogram-bot-for-u.herokuapp.com/'
-WEBHOOK_URL = f"{WEBHOOK_HOST}"
+WEBHOOK_PORT = 443
+WEBHOOK_URL = f"{WEBHOOK_HOST}:{WEBHOOK_PORT}"
 WEBHOOK_PATH = ''
 WEBAPP_HOST = 'localhost'
-WEBAPP_PORT = 5000
+WEBAPP_PORT = 3001
 token = '5000663222:AAFZvEvHl9EmyMVen2N52UJCqQiX3MwsaY8'
 bot = Bot(token)
 dp = Dispatcher(bot, storage=storage)
